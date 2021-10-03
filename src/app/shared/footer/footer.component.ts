1fbdf0f5f660048360f4367c02efdd90b9843ce4
Romulo <romulocoutinho16@gmail.com>
2021-10-02 22:13:54 -0300

    added carousel to detail and edit pages

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Dealership } from 'src/app/models/dealership.model';
+import { DealershipService } from 'src/app/services/dealership.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,23 +11,24 @@ import { Dealership } from 'src/app/models/dealership.model';
 export class FooterComponent implements OnInit {
 
   public dealership: Dealership = {
-    "adressGoogleMapsUrl": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3747.5713407937596!2d-44.58052791679989!3d-20.068389846766642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa7334eaf784437%3A0x112d321547a80b3a!2sRX%20Multimarcas!5e0!3m2!1spt-BR!2sbr!4v1624483226913!5m2!1spt-BR!2sbr",
-    "telephone": "(37)3241-5001",
-    "facebookUrl": "#",
-    "instagramUrl": "https://www.instagram.com/multimarcasrx/",
-    "whatsappUrl": "#",
-    "youtubeUrl": "#",
-    "twitterUrl": "#",
-    "mondayToFridayOpening": "08:00",
-    "mondayToFridayClosing": "18:00",
-    "saturdayOpening": "09:00",
-    "saturdayClosing": "12:00",
-    "address": "Teste 3"
+    "address": "",
+    "adressGoogleMapsUrl": "",
+    "telephone": "",
+    "facebookUrl": "",
+    "instagramUrl": "",
+    "whatsappUrl": "",
+    "youtubeUrl": "",
+    "twitterUrl": "",
+    "mondayToFridayOpening": "",
+    "mondayToFridayClosing": "",
+    "saturdayOpening": "",
+    "saturdayClosing": ""
   };
 
-  constructor() { }
+  constructor(private dealershipService: DealershipService) { }
 
   ngOnInit(): void {
+    this.dealership = this.dealershipService.getDealership();
   }
 
 }
--- src/app/views/admin/admin.component.ts
+++ src/app/views/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Dealership } from 'src/app/models/dealership.model';
+import { DealershipService } from 'src/app/services/dealership.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  public dealership: Dealership = {
+    "address": "",
+    "adressGoogleMapsUrl": "",
+    "telephone": "",
+    "facebookUrl": "",
+    "instagramUrl": "",
+    "whatsappUrl": "",
+    "youtubeUrl": "",
+    "twitterUrl": "",
+    "mondayToFridayOpening": "",
+    "mondayToFridayClosing": "",
+    "saturdayOpening": "",
+    "saturdayClosing": ""
+  };
+
+  public isReadOnly: boolean = true;
+
+  constructor(private router: Router,
+              private dealershipService: DealershipService) { }
 
   ngOnInit(): void {
+    this.dealership = this.dealershipService.getDealership();
+  }
+
+  public toggleEdit(){
+    this.isReadOnly ? this.isReadOnly = false : this.isReadOnly = true;
+  }
+
+  public edit(form: any){
+    this.dealershipService.updateDealership(this.dealership);
+    this.toggleEdit();
   }
 
 }
